47e6f4a23d32ae64b97aefa7e2585c8a5248b8fc
Henry Wietfeldt <henry.wietfeldt@gmail.com>
2025-10-15 18:21:57 -0400

    added heat flux calculation

--- mirror_heat_flux/plot_popcons.py
+++ mirror_heat_flux/plot_popcons.py
@@ -1,2 +1,19 @@
 """
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_heat_flux(B0, Bw):
+    """
+    Returns parallel heat flux on wall in MW/m^2
+    """
+    # TODO: Add fixed parameters to config.toml file
+    eta_b = 0.8
+    pb_mw = 25 # [MW]
+    q_phy = 0.25
+    eb_100keV = 1
+    n25 = 1
+    heat_flux = eta_b * pb_mw * (1 + q_phy / 5) * B0 * Bw
+    heat_flux = heat_flux / (2*np.pi * n25**2 * np.sqrt(eb_100keV))
+    return heat_flux
